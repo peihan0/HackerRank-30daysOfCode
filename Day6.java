fcc7ea1677081eae3db7ca77df77832e4fa963bb
peihan0 <ty9722218@gmail.com>
2022-12-04 19:48:14 +0800

    commit current

---
+++ Day6.java
@@ -0,0 +1,24 @@
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner scn = new Scanner(System.in);
+        int times = scn.nextInt();
+        scn.nextLine();
+        while(times-- > 0){
+            String str = scn.nextLine();
+            
+            for(int i = 0 ; i < str.length() ; i += 2){
+                System.out.print(str.charAt(i));
+            }
+
+            System.out.print(" ");
+    
+            for(int i = 1 ; i < str.length() ; i += 2){
+                System.out.print(str.charAt(i));
+            }
+            System.out.println("");            
+        }
+        scn.close();
+    }
+}
---
+++ Day7.java
@@ -0,0 +1,26 @@
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+        List<Integer> arr = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+            .map(Integer::parseInt)
+            .collect(toList());
+            
+            for(int i = n-1 ; i >= 0 ; i--){
+                if(i == n - 1){
+                    int cur = arr.get(i);
+                    System.out.print(cur);    
+                }else{
+                    int cur = arr.get(i);
+                    System.out.print(" " + cur);  
+                    
+                }
+               
+            }
+
+        bufferedReader.close();
+    }
+}
